4addc7f7b71d39261e8e2b11e9f918d2bea2c7c4
あさづけ <quusan3@gmail.com>
2019-07-14 01:11:27 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 'use strict';
 
+const fibMap = new Map();
+
+fibMap.set(0,0);
+fibMap.set(1,1);
+
 function fib(n) {
-  if (n === 0) {
-    return 0;
-  } else if (n === 1) {
-    return 1;
+  if(fibMap.has(n)){
+    return fibMap.get(n);
   }
-  return fib(n - 2) + fib(n - 1);
+  const value = fib(n - 2) + fib(n - 1);
+  fibMap.set(n,value);
+  return value;
 }
+
+
 
 const length = 40;
 
